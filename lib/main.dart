9d75d383e3b6a4391cd452eed1a5fbf5c3b8d17f
Usman Abubakar <104586608+Usmandankama@users.noreply.github.com>
2026-05-10 14:22:18 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:expense_mate_flutter/bindings/home_bindings.dart';
 import 'package:expense_mate_flutter/bindings/reciept_binding.dart';
+import 'package:expense_mate_flutter/controllers/auth_controller.dart';
+import 'package:expense_mate_flutter/controllers/expense_controller.dart';
 import 'package:expense_mate_flutter/controllers/home_controller.dart';
+import 'package:expense_mate_flutter/controllers/income_controller.dart';
+import 'package:expense_mate_flutter/controllers/user_controller.dart';
+import 'package:expense_mate_flutter/firebase_options.dart';
 import 'package:expense_mate_flutter/screens/RecieptScanner/receipt_scanner.dart';
+import 'package:expense_mate_flutter/screens/expenses/expense_screen.dart';
 import 'package:expense_mate_flutter/screens/home/home_screen.dart';
 import 'package:expense_mate_flutter/screens/home/home_shell.dart';
+import 'package:expense_mate_flutter/screens/income/income_screen.dart';
+import 'package:expense_mate_flutter/screens/login/login_screen.dart';
+import 'package:expense_mate_flutter/screens/register/register_screen.dart';
 import 'package:expense_mate_flutter/screens/splash_screen/splash_timer.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
@@ -13,6 +23,13 @@ import 'package:get_storage/get_storage.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await GetStorage.init(); // Initialize local storage
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  ); // Initialize Firebase
+  Get.put(UserController());
+  Get.put(AuthController());
+  Get.put(IncomeController());
+  Get.put(ExpenseController());
   Get.put(HomeController());
   runApp(const MyApp());
 }
@@ -41,6 +58,10 @@ class MyApp extends StatelessWidget {
               binding:
                   ReceiptBinding(), // This injects your OCR & Backend services!
             ),
+            GetPage(name: "/login", page: () => const LoginScreen()),
+            GetPage(name: "/register", page: () => const RegisterScreen()),
+            GetPage(name: "/expenses", page: () => const ExpenseScreen()),
+            GetPage(name: "/income", page: () => const IncomeScreen()),
           ],
           debugShowCheckedModeBanner: false,
           home: _decideStartingScreen(),
@@ -54,7 +75,6 @@ class MyApp extends StatelessWidget {
     bool isFirstTime = box.read("isFirstTime") ?? true; // Default is true
 
     if (isFirstTime) {
-      // Set first-time flag to false after showing splash screen
       box.write("isFirstTime", false);
       return const SplashTimer();
     } else {
